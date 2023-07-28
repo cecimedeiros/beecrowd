40c45ea66fcdeffea5ab34a1eddcf2f4a6064501
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-07-27 23:25:08 -0300

    Update 1059.java

--- Java/1059.java
+++ Java/1059.java
@@ -1,10 +1,10 @@
 /*
 Faça um programa que mostre os números pares entre 1 e 100, inclusive.
 
-Entrada
+Entrada:
 Neste problema extremamente simples de repetição não há entrada.
 
-Saída
+Saída:
 Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha.
 */
 
